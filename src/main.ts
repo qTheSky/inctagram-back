e738c5113e94eadb44bbfa92e4302c9ae71ca147
qTheSky <smirnov.mic@yandex.ru>
2023-04-09 16:54:52 +0300

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './modules/shared/filters/exception.filter';
 import cookieParser from 'cookie-parser';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { get } from 'http';
+import { createWriteStream } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -49,6 +51,35 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
   //swagger
+
+  //for swagger and vercel
+  const serverUrl = 'http://localhost:3000';
+  // get the swagger json file (if app is running in development mode)
+  if (process.env.NODE_ENV === 'development') {
+    // write swagger ui files
+    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
+    });
+
+    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
+    });
+
+    get(
+      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
+      function (response) {
+        response.pipe(
+          createWriteStream('swagger-static/swagger-ui-standalone-preset.js')
+        );
+      }
+    );
+
+    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
+      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
+    });
+  }
+  //for swagger
+  //for swagger and vercel
 
   await app.listen(3000);
 }
---
+++ swagger-static/swagger-ui-init.js
@@ -0,0 +1,349 @@
+
+window.onload = function() {
+  // Build a system
+  let url = window.location.search.match(/url=([^&]+)/);
+  if (url && url.length > 1) {
+    url = decodeURIComponent(url[1]);
+  } else {
+    url = window.location.origin;
+  }
+  let options = {
+  "swaggerDoc": {
+    "openapi": "3.0.0",
+    "paths": {
+      "/auth/registration": {
+        "post": {
+          "operationId": "AuthController_registration",
+          "summary": "Registration in the system. Email with confirmation code will be send to passed email address.",
+          "parameters": [],
+          "requestBody": {
+            "required": true,
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/RegisterDto"
+                }
+              }
+            }
+          },
+          "responses": {
+            "204": {
+              "description": "Input data is accepted. Email with confirmation code will be send to passed email address"
+            },
+            "400": {
+              "description": "If the inputModel has incorrect values (in particular if the user with the given email or login already exists)",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "errorsMessages": [
+                        {
+                          "message": "string",
+                          "field": "string"
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            },
+            "429": {
+              "description": "More than 5 attempts from one IP-address during 10 seconds"
+            }
+          },
+          "tags": [
+            "Auth"
+          ]
+        }
+      },
+      "/auth/login": {
+        "post": {
+          "operationId": "AuthController_login",
+          "summary": "Try login user to the system",
+          "parameters": [],
+          "requestBody": {
+            "required": true,
+            "description": "Example request body",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/LoginDto"
+                }
+              }
+            }
+          },
+          "responses": {
+            "200": {
+              "description": "Returns JWT accessToken (expired after 10 minutes) in body and JWT refreshToken in cookie (http-only, secure) (expired after 30 days).",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "accessToken": "string"
+                    }
+                  }
+                }
+              }
+            },
+            "400": {
+              "description": "If the inputModel has incorrect values",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "errorsMessages": [
+                        {
+                          "message": "string",
+                          "field": "string"
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            },
+            "401": {
+              "description": "If the password or login is wrong"
+            },
+            "429": {
+              "description": "More than 5 attempts from one IP-address during 10 seconds"
+            }
+          },
+          "tags": [
+            "Auth"
+          ]
+        }
+      },
+      "/auth/confirm-email": {
+        "post": {
+          "operationId": "AuthController_confirmUserEmail",
+          "summary": "Confirm registration.",
+          "parameters": [],
+          "requestBody": {
+            "required": true,
+            "content": {
+              "application/json": {
+                "schema": {
+                  "$ref": "#/components/schemas/EmailConfirmDto"
+                }
+              }
+            }
+          },
+          "responses": {
+            "204": {
+              "description": "Email was verified. Account was activated"
+            },
+            "400": {
+              "description": "If the confirmation code is incorrect, expired or already been applied",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "errorsMessages": [
+                        {
+                          "message": "string",
+                          "field": "string"
+                        }
+                      ]
+                    }
+                  }
+                }
+              }
+            },
+            "429": {
+              "description": "More than 5 attempts from one IP-address during 10 seconds"
+            }
+          },
+          "tags": [
+            "Auth"
+          ]
+        }
+      },
+      "/auth/refresh-token": {
+        "post": {
+          "operationId": "AuthController_refreshToken",
+          "summary": "Generate new pair of access and refresh tokens (in cookie client must send correct refreshToken that will be revoked after refreshing). ",
+          "parameters": [],
+          "responses": {
+            "200": {
+              "description": "Returns JWT accessToken (expired after 8 hours) in body and JWT refreshToken in cookie (http-only, secure) (expired after 30days).",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "accessToken": "string"
+                    }
+                  }
+                }
+              }
+            },
+            "401": {
+              "description": "If the JWT refreshToken inside cookie is missing, expired or incorrect"
+            }
+          },
+          "tags": [
+            "Auth"
+          ]
+        }
+      },
+      "/auth/logout": {
+        "post": {
+          "operationId": "AuthController_logout",
+          "summary": "In cookie client must send correct refreshToken that will be revoked",
+          "parameters": [],
+          "responses": {
+            "204": {
+              "description": "No content"
+            },
+            "401": {
+              "description": "If the JWT refreshToken inside cookie is missing, expired or incorrect"
+            }
+          },
+          "tags": [
+            "Auth"
+          ]
+        }
+      },
+      "/auth/me": {
+        "get": {
+          "operationId": "AuthController_getAuthUserData",
+          "summary": "Get information about current user",
+          "parameters": [],
+          "responses": {
+            "200": {
+              "description": "Success",
+              "content": {
+                "application/json": {
+                  "schema": {
+                    "example": {
+                      "email": "string",
+                      "userName": "string",
+                      "userId": "string"
+                    }
+                  }
+                }
+              }
+            },
+            "401": {
+              "description": "Unauthorized"
+            }
+          },
+          "tags": [
+            "Auth"
+          ],
+          "security": [
+            {
+              "bearer": []
+            }
+          ]
+        }
+      }
+    },
+    "info": {
+      "title": "INCTAGRAM API",
+      "description": "here will be some description",
+      "version": "1.0",
+      "contact": {}
+    },
+    "tags": [],
+    "servers": [],
+    "components": {
+      "schemas": {
+        "RegisterDto": {
+          "type": "object",
+          "properties": {
+            "email": {
+              "type": "string",
+              "description": "User email",
+              "example": "user@example.com",
+              "format": "email"
+            },
+            "userName": {
+              "type": "string",
+              "description": "User name",
+              "example": "John",
+              "minLength": 3,
+              "maxLength": 10
+            },
+            "password": {
+              "type": "string",
+              "description": "User password",
+              "example": "string",
+              "minLength": 6,
+              "maxLength": 20
+            }
+          },
+          "required": [
+            "email",
+            "userName",
+            "password"
+          ]
+        },
+        "LoginDto": {
+          "type": "object",
+          "properties": {
+            "userNameOrEmail": {
+              "type": "string"
+            },
+            "password": {
+              "type": "string"
+            }
+          },
+          "required": [
+            "userNameOrEmail",
+            "password"
+          ]
+        },
+        "EmailConfirmDto": {
+          "type": "object",
+          "properties": {
+            "code": {
+              "type": "string",
+              "description": "Confirmation code",
+              "example": "someUUIDdsajkdsa-dsad-as-das-ddsa",
+              "format": "email"
+            }
+          },
+          "required": [
+            "code"
+          ]
+        }
+      }
+    }
+  },
+  "customOptions": {}
+};
+  url = options.swaggerUrl || url
+  let urls = options.swaggerUrls
+  let customOptions = options.customOptions
+  let spec1 = options.swaggerDoc
+  let swaggerOptions = {
+    spec: spec1,
+    url: url,
+    urls: urls,
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    layout: "StandaloneLayout"
+  }
+  for (let attrname in customOptions) {
+    swaggerOptions[attrname] = customOptions[attrname];
+  }
+  let ui = SwaggerUIBundle(swaggerOptions)
+
+  if (customOptions.initOAuth) {
+    ui.initOAuth(customOptions.initOAuth)
+  }
+
+  if (customOptions.authAction) {
+    ui.authActions.authorize(customOptions.authAction)
+  }
+  
+  window.ui = ui
+}
