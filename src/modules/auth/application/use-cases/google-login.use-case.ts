2b51f2b33792b85f9296fb7b3dee8e045a610cf5
qTheSky <smirnov.mic@yandex.ru>
2023-04-14 16:02:11 +0300

    add random login generator

--- src/modules/auth/application/use-cases/google-login.use-case.ts
+++ src/modules/auth/application/use-cases/google-login.use-case.ts
@@ -7,8 +7,8 @@ import {
   UsersRepository,
 } from '../../../users/infrastructure';
 import { UserEntity } from '../../../users/entities';
-import { randomUUID } from 'crypto';
 import { ILoginTokens } from '../../interfaces/login-tokens.interface';
+import randomWords from 'random-words';
 
 export class GoogleLoginCommand {
   constructor(
@@ -54,7 +54,7 @@ export class GoogleLoginUseCase implements ICommandHandler<GoogleLoginCommand> {
       {
         login: isGoogleDisplayNameUnique
           ? googleUser.displayName
-          : randomUUID(),
+          : await this.generateRandomUniqueLogin(),
         password: 'this user uses google oauth',
         email: googleUser.email,
         passwordHash: 'this user uses google oauth',
@@ -65,5 +65,29 @@ export class GoogleLoginUseCase implements ICommandHandler<GoogleLoginCommand> {
     return await this.commandBus.execute(
       new LoginCommand(savedUser, sessionMetadata)
     );
+  }
+
+  /**
+   * this function will create random UNIQUE login
+   */
+  private async generateRandomUniqueLogin() {
+    let login: string;
+    let user;
+
+    // The 'do' block will be executed at least once and then the loop will continue
+    // as long as the condition specified in the 'while' is true.
+    do {
+      // Generate a single random word
+      login = randomWords({ exactly: 1, join: '' });
+
+      // Find user by generated login
+      user = await this.usersQueryRepository.findUserByLoginOrEmail(login);
+
+      // If 'user' is not null, it means the generated login is already in use.
+      // In that case, the loop will continue, and a new login will be generated.
+    } while (user);
+
+    // When the loop exits, it means we've found a unique login
+    return login.charAt(0).toUpperCase() + login.slice(1); // it makes first letter to uppercase
   }
 }
