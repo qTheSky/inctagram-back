0f5e8d9d67d770de16ecde2ede0daa810575901e
qTheSky <smirnov.mic@yandex.ru>
2023-04-26 19:12:38 +0300

    cors for storybook

--- src/config/originSetup.ts
+++ src/config/originSetup.ts
@@ -1,7 +1,11 @@
 import { INestApplication } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
-const origin = ['http://localhost:3000', 'https://inctagram.online'];
+const origin = [
+  'http://localhost:3000',
+  'https://inctagram.online',
+  'http://localhost:6006',
+];
 
 const getCorsOptions = (origin: string[]): CorsOptions => ({
   origin,
