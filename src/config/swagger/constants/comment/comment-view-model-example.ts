cc4e9553ebc46f3c62d07f6b363e900f7d1269ce
100CallsToEurop <piaterik123123@mail.ru>
2023-05-27 09:58:39 +0300

    fix swagger comments

--- src/config/swagger/constants/comment/comment-view-model-example.ts
+++ src/config/swagger/constants/comment/comment-view-model-example.ts
@@ -1,12 +1,14 @@
-import {
-  CommentViewModel,
-  LikesInfoViewModel,
-} from '../../../../modules/comments/api/models/view/comment.view.model';
+import { LikeStatus } from '../../../../modules/shared/classes/abstract.like-info.class';
+import { CommentViewModel } from '../../../../modules/comments/api/models/view/comment.view.model';
 
 export const commentViewModelExample: CommentViewModel = {
   id: 'string',
   content: 'string',
   commentatorInfo: { userId: 'string', userLogin: 'string' },
   createdAt: '2023-03-13T12:42:19.885Z',
-  likesInfo: new LikesInfoViewModel(),
+  likesInfo: {
+    likesCount: 0,
+    dislikesCount: 0,
+    myStatus: LikeStatus.NONE,
+  },
 };
