ef34adccb4106d1b8688d8aa1cd7c0eda4787b1c
100CallsToEurop <piaterik123123@mail.ru>
2023-05-27 12:19:44 +0300

    add like posts

--- src/modules/posts/api/dto/view/PostViewModel.ts
+++ src/modules/posts/api/dto/view/PostViewModel.ts
@@ -20,39 +20,39 @@ export class ExtendedLikesInfoViewModel {
 
 export class PostViewModel {
   @ApiProperty({
-    description: 'id',
-    example: '3123213123',
-    type: 'string',
+    description: "id",
+    example: "3123213123",
+    type: "string",
   })
   id: string;
   @ApiProperty({
-    description: 'photos url',
-    example: '[https://url.com/photo1.jpg, https://url.com/photo1.jpg]',
-    type: 'string[]',
-    format: 'url',
+    description: "photos url",
+    example: "[https://url.com/photo1.jpg, https://url.com/photo1.jpg]",
+    type: "string[]",
+    format: "url",
   })
   photos: string[];
   @ApiProperty({
-    description: 'post description',
-    example: 'frontend noobs',
-    type: 'string',
+    description: "post description",
+    example: "frontend noobs",
+    type: "string",
   })
   description: string;
   @ApiProperty({
-    description: 'date when post was created',
-    example: new Date('2023-04-10T16:20:10.847Z'),
-    type: 'string',
+    description: "date when post was created",
+    example: new Date("2023-04-10T16:20:10.847Z"),
+    type: "string",
   })
   createdAt: Date;
   @ApiProperty({
-    description: 'date when post was created',
-    example: new Date('2023-04-10T16:20:10.847Z'),
-    type: 'string',
+    description: "date when post was created",
+    example: new Date("2023-04-10T16:20:10.847Z"),
+    type: "string",
   })
   updatedAt: Date;
   @ApiProperty({
-    description: 'post likes info',
-    type: 'string',
+    description: "post likes info",
+    type: ExtendedLikesInfoViewModel,
   })
   extendedLikesInfo: ExtendedLikesInfoViewModel;
 }
